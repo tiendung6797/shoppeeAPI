d7d79865853df39096317fd263c8bb2f6e47d6e1
Dikuto <baduy9780@gmail.com>
2019-04-22 13:20:21 +0700

    duy commit upload file

--- src/main/java/shoppee/com/ShoppeeApiApplication.java
+++ src/main/java/shoppee/com/ShoppeeApiApplication.java
@@ -2,8 +2,12 @@ package shoppee.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import shoppee.com.property.FileStorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({FileStorageProperties.class})
 public class ShoppeeApiApplication {
 
 	public static void main(String[] args) {
